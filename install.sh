8885a7397548986d361725337672b933dd343510
lamanotrama <lamanotrama@gmail.com>
2012-09-11 18:58:26 +0900

    -nとか-fとかつけて、フォースなインストールができると嬉しい。

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ set -e
 
 DIR=$(dirname $(readlink -f $0))
 cd $DIR
-./cpanm -L extlib --mirror=$DIR/cpan-mirror/ --mirror-only --installdeps .
+./cpanm -L extlib --mirror=$DIR/cpan-mirror/ --mirror-only --installdeps . "$@"
 
