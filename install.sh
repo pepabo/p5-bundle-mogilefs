4567f5d9b5fda8eb8fe18442a8bfb6da3cd54852
Hiroya Ito <hiroyan@gmail.com>
2015-11-18 23:24:11 +0900

    readlink が GNU coreutils 実装に依存しているので OSX で動かない。 readlink する必要ないなら回避可能なので -L で分岐を入れる

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-DIR=$(dirname $(readlink -f $0))
+if [ -L $0 ]; then
+    DIR=$(dirname $(readlink -f $0))
+else
+    DIR=$(dirname $0)
+fi
+
 cd $DIR
 ./cpanm -L extlib --mirror=$(pwd)/cpan-mirror/ --mirror-only --installdeps . "$@"
 
